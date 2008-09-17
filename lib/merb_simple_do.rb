8d22bf3c364995a86c2c881a96b4e482b809073e
Coda Hale <coda.hale@gmail.com>
2008-09-16 18:11:05 -0700

    Added logging.

--- lib/merb_simple_do.rb
+++ lib/merb_simple_do.rb
@@ -3,6 +3,8 @@ if defined?(Merb::Plugins)
   require "simple_do"
   
   Merb::BootLoader.before_app_loads do
+    Merb.logger.info("Connecting to database...")
     ::DB = DataObjects::Simple.new(Merb.dir_for(:config) / "database.yml", Merb.environment)
+    ::DB.logger = Merb.logger
   end
 end
